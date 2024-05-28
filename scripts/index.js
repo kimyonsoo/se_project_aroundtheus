296d1fb83d4b4ea2d29900f46882f2686cf2a73d
Soo Kim <ikimyonsoo@berkeley.edu>
2024-05-27 20:12:19 -0700

    Refactoring

--- scripts/index.js
+++ scripts/index.js
@@ -29,3 +29,46 @@ const initialCards = [
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lago.jpg",
   },
 ];
+
+/* Elements */
+
+const profileEditButton = document.querySelector("#profile-edit-button");
+const profileEditModal = document.querySelector("#profile-edit-modal");
+const profileEditModalCloseButton = document.querySelector(
+  "#profile-edit-modal-close-button"
+);
+const profileTitle = document.querySelector(".profile__title");
+const profileDescription = document.querySelector(".profile__description");
+
+const profileEditForm = profileEditModal.querySelector(".modal__form");
+const profileTitleInput = document.querySelector("#profile-title-input");
+const profileDescriptionInput = document.querySelector(
+  "#profile-description-input"
+);
+
+/* Functions */
+
+function closePopUp() {
+  profileEditModal.classList.remove("modal_opened");
+}
+/* Event Handlers */
+
+function handleProfileEditSubmit(evt) {
+  evt.preventDefault();
+  profileTitle.textContent = profileTitleInput.value;
+  profileDescription.textContent = profileDescriptionInput.value;
+  closePopUp();
+}
+
+/* Event Listeners */
+
+profileEditButton.addEventListener("click", () => {
+  profileTitleInput.value = profileTitle.textContent;
+  profileDescriptionInput.value = profileDescription.textContent;
+
+  profileEditModal.classList.add("modal_opened");
+});
+
+profileEditModalCloseButton.addEventListener("click", closePopUp);
+
+profileEditForm.addEventListener("submit", handleProfileEditSubmit);
